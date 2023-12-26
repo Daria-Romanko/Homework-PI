7d9dccacf801bc0e0c3b65d5ee9794a1880fcd77
Daria-Romanko <115109486+Daria-Romanko@users.noreply.github.com>
2023-12-26 15:19:22 +0300

    test 3

--- HomeworkPI/fail.py
+++ HomeworkPI/fail.py
@@ -1,24 +1,7 @@
-import requests
-
-url = 'https://api.tracker.yandex.net/v2/issues'
-
-headers = {
-    'Authorization': 'Bearer your_access_token',
-    'Content-Type': 'application/json'
-}
-
-
-data = {
-    'queue': 'TeamCity Build Fails',
-    'summary': 'Build failed',
-    'description': 'Please fix it.',
-    'assignee': 'Daria190404@yandex.ru' 
-}
-
-
-response = requests.post(url, headers=headers, json=data)
-
-if response.status_code == 201:
-    print('Task created successfully in Yandex Tracker')
-else:
-    print('Failed to create task in Yandex Tracker')
+from yandex_tracker_client import TrackerClient
+client = TrackerClient(token = 't1.9euelZqMipmYjcnHmpzLi5HMlJuMxu3rnpWazZ6aypWVjYrNnJaJz46Vy87l8_ceRFVT-e8_eRRf_d3z915yUlP57z95FF_9zef1656VmpaajcvNlI2Wj52eiY2Nx8ia7_zF656VmpaajcvNlI2Wj52eiY2Nx8ia.1W0FhYKz8_STJM8LntypZylDAwZXBkKn7hlatR2Y6jbt55Lps2Vksp-AKdfCkMN4oZGdKwE5HcVMnpgbgoPxDA', cloud_org_id = 'bpf2nacm2mbsf4s7d2ml')
+client.issues.create(
+    queue = 'TEAMCITYBUILDFA',
+    summary = 'Build fail',
+    assignee = 'TEAMCITYBUILDFA'
+)
