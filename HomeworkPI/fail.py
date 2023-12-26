cdf2678a1ca01c9b5aac72f117cf7c917459e8fe
Daria-Romanko <115109486+Daria-Romanko@users.noreply.github.com>
2023-12-26 17:57:33 +0300

    AAAAAAAAAAAAAA

--- HomeworkPI/fail.py
+++ HomeworkPI/fail.py
@@ -1,6 +1,6 @@
 from yandex_tracker_client import TrackerClient
 
-client = TrackerClient(token = 't1.9euelZrMjsiKxo2Yx87NyY_OicyTie3rnpWazZ6aypWVjYrNnJaJz46Vy87l8_cmLVRT-e8nOx1u_N3z92ZbUVP57yc7HW78zef1656VmpKRnZ7GkcedmpmPkcvNycnG7_zF656VmpKRnZ7GkcedmpmPkcvNycnG.IrF2MMj32zy1TJvvrFMrpljvSitBUc2U19rQ3B5yVKMv4Tb_7S71ZSiE2dLvZUcv2l8sTMWxk1UhuUHPZPRMDw', cloud_org_id = 'bpf2nacm2mbsf4s7d2ml')
+client = TrackerClient(token = 'y0_AgAAAAA_Po2UAATuwQAAAAD190GIqbXrdLKKSHqsz5VJVLgN-3os8Wc', cloud_org_id = 'bpf2nacm2mbsf4s7d2ml')
 
 client.issues.create(
     queue = 'TEAMCITYBUILDFA',
