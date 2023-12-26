712a4c418ea650d3228497a59ff44fc8bbb0ca03
Daria-Romanko <115109486+Daria-Romanko@users.noreply.github.com>
2023-12-26 17:56:03 +0300

    aaaa

--- HomeworkPI/fail.py
+++ HomeworkPI/fail.py
@@ -1,6 +1,6 @@
 from yandex_tracker_client import TrackerClient
 
-client = TrackerClient(token = 't1.9euelZqMipmYjcnHmpzLi5HMlJuMxu3rnpWazZ6aypWVjYrNnJaJz46Vy87l8_cTMlRT-e9KTEkK_t3z91NgUVP570pMSQr-zef1656VmpqJzMjJj5yTnpqQj5iTiYqT7_zF656VmpqJzMjJj5yTnpqQj5iTiYqT.SLglAlV3k5nYFn7I8JsFHCIS-fTu7idEZ3APS5LZvOm5QT_n98Z9WavVOn0B0jv9XgE_0TLMsj30-ZP_44VzDA', cloud_org_id = 'bpf2nacm2mbsf4s7d2ml')
+client = TrackerClient(token = 't1.9euelZrMjsiKxo2Yx87NyY_OicyTie3rnpWazZ6aypWVjYrNnJaJz46Vy87l8_cmLVRT-e8nOx1u_N3z92ZbUVP57yc7HW78zef1656VmpKRnZ7GkcedmpmPkcvNycnG7_zF656VmpKRnZ7GkcedmpmPkcvNycnG.IrF2MMj32zy1TJvvrFMrpljvSitBUc2U19rQ3B5yVKMv4Tb_7S71ZSiE2dLvZUcv2l8sTMWxk1UhuUHPZPRMDw', cloud_org_id = 'bpf2nacm2mbsf4s7d2ml')
 
 client.issues.create(
     queue = 'TEAMCITYBUILDFA',
